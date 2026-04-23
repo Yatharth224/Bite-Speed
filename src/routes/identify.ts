f30e7d650a8fff6762a070760dc84f2efc4771b7
Your Name <yatharthchouhan22@gmail.com>
2026-04-23 17:54:10 +0530

    add comments to routes

--- src/routes/identify.ts
+++ src/routes/identify.ts
@@ -3,6 +3,7 @@ import { identifyUser } from "../controllers/identifyController"
 
 const router = Router()
 
+// any POST request to /identify goes to the identifyUser function
 router.post("/identify", identifyUser)
 
 export default router
