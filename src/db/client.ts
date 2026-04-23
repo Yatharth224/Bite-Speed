54149c85fe90812d21b5b17db525df89e19a7ab8
Your Name <yatharthchouhan22@gmail.com>
2026-04-23 17:53:15 +0530

    detailed comments in clients.ts

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client"
 
+// single db connection for the whole app
+// we dont create this multiple times, just import this wherever we need db access
 const db = new PrismaClient()
 
 export default db
